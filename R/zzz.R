eab800d27f22970dd88ad042e17e0c5340268b68
Zuguang Gu <jokergoo@gmail.com>
2023-09-23 13:30:50 +0200

    update

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach = function(libname, pkgname) {
 
-  	msg = paste0("Gene summaries were retrieved from RefSeq database release 217 (December 21, 2022).")
+  	msg = paste0("Gene summaries were retrieved from RefSeq database release 220 (December 21, 2022).")
 
     packageStartupMessage(msg)
 }
